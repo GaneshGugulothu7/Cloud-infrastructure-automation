fc53aec62e2ce0a70b08cd2cce5a7265620dd36e
Rupa <gugulothuganesh06@gmail.com>
2026-03-31 10:31:41 +0530

    cicd pipelines

---
+++ Cloud-Infrastrecture-automation-using-terraform-master/variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+  default = "West US 3"
+}
+
+variable "resource_group_name" {
+  default = "Finance-APP-RG"
+}
+
+variable "app_service_name" {
+  default = "Finance-service-app"
+}
+
+variable "cosmos_account_name" {
+  default = "financecosmos123456"
+}
+ 
